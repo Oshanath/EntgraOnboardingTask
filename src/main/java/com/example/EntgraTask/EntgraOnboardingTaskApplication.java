d26b4be290a349df887d747e4734e179fe2e4994
Oshanath Rajawasam <oshanath@entgra.io>
2023-01-11 11:21:59 +0530

    initial commit

---
+++ src/main/java/com/example/EntgraTask/EntgraOnboardingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.EntgraTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntgraOnboardingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntgraOnboardingTaskApplication.class, args);
+	}
+
+}
